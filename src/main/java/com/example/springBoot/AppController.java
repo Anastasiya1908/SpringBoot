762d016d5bf190ac2e0410722655a0c37b875840
Anastasiya Sliunkova <asynkova@marathonbet.ru>
2020-01-30 15:57:50 +0300

    Добавление bean

--- src/main/java/com/example/springBoot/AppController.java
+++ src/main/java/com/example/springBoot/AppController.java
@@ -1,8 +1,13 @@
 package com.example.springBoot;
 
-import org.springframework.stereotype.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -10,12 +15,49 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class AppController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    static final Logger LOGGER = LoggerFactory.getLogger(AppController.class);
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong COUNTER = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "email") String email) {
+
+        LOGGER.info("Received email: " + email);
+        return new Greeting(COUNTER.incrementAndGet(), String.format(TEMPLATE, email));
+
     }
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Empty mapping";
+    }
+
+    @RequestMapping("/emit/error")
+    @ResponseBody
+    String error() {
+        LOGGER.info("Emit as error");
+        rabbitTemplate.convertAndSend("error", "Error");
+        return "Emit as error";
+    }
+
+    @RequestMapping("/emit/info")
+    @ResponseBody
+    String info() {
+        LOGGER.info("Emit as info");
+        rabbitTemplate.convertAndSend("info", "info");
+        return "Emit as info";
+    }
+
+    @RequestMapping("/emit/warning")
+    @ResponseBody
+    String warning() {
+        LOGGER.info("Emit as warning");
+        rabbitTemplate.convertAndSend("warning", "Warning");
+        return "Emit as warning";
+    }
 }
--- src/main/java/com/example/springBoot/RabbitConfiguration.java
+++ src/main/java/com/example/springBoot/RabbitConfiguration.java
@@ -1,4 +1,74 @@
+
 package com.example.springBoot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitConfiguration {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory =
+                new CachingConnectionFactory("localhost");
+        return connectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory());
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setExchange("exchange-1");
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue myQueue1() {
+        return new Queue("query-1-1");
+    }
+
+    @Bean
+    public Queue myQueue2() {
+        return new Queue("query-1-2");
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("exchange-1");
+    }
+
+    @Bean
+    public Binding errorBinding1() {
+        return BindingBuilder.bind(myQueue1()).to(directExchange()).with("error");
+    }
+
+    @Bean
+    public Binding errorBinding2() {
+        return BindingBuilder.bind(myQueue2()).to(directExchange()).with("error");
+    }
+
+    @Bean
+    public Binding infoBinding() {
+        return BindingBuilder.bind(myQueue2()).to(directExchange()).with("info");
+    }
+
+    @Bean
+    public Binding warningBinding() {
+        return BindingBuilder.bind(myQueue2()).to(directExchange()).with("warning");
+    }
+
 }
+
