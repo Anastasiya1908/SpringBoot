82acd868e4ddf1d190441a9700b32f482523aff3
Anastasiya Sliunkova <asynkova@marathonbet.ru>
2020-01-31 09:38:32 +0300

    Добавлены изменения бинов

--- src/main/java/com/example/springBoot/RabbitConfiguration.java
+++ src/main/java/com/example/springBoot/RabbitConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,24 +17,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfiguration {
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
-        return connectionFactory;
-    }
 
     @Bean
-    public AmqpAdmin amqpAdmin() {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory());
-        return rabbitAdmin;
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setExchange("exchange-1");
-        return rabbitTemplate;
+    public MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory){
+        return new SimpleMessageListenerContainer(connectionFactory);
     }
 
     @Bean
