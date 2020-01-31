bd9f722719b3fc6fc85cfca48a2fa46f3ea1955d
Anastasiya Sliunkova <asynkova@marathonbet.ru>
2020-01-31 15:47:40 +0300

    Внесены изменения в контроллере

--- src/main/java/com/example/springBoot/AppController.java
+++ src/main/java/com/example/springBoot/AppController.java
@@ -1,14 +1,10 @@
 package com.example.springBoot;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -16,13 +12,12 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class AppController {
     @Autowired
-    EmailService emailService;
+    private EmailService emailService;
 
     static final Logger LOGGER = LoggerFactory.getLogger(AppController.class);
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public void Greeting(@RequestParam(value = "email") String email) {
+    public void greeting(@RequestParam(value = "email") String email) {
         emailService.sendEmail(email);
         LOGGER.info("Received email: " + email);
     }
